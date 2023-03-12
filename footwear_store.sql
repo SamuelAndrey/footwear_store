1ac161617d778fb0aa99e77c30cb22c7033247fd
samuel <samuelandrey670@gmail.com>
2023-03-12 22:19:17 +0700

    Add table invoice & pesanan

--- footwear_store.sql
+++ footwear_store.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 11, 2023 at 08:55 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Mar 12, 2023 at 04:03 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 7.4.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,7 +35,7 @@ CREATE TABLE `tb_barang` (
   `harga` int(11) NOT NULL,
   `stok` int(4) NOT NULL,
   `gambar` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tb_barang`
@@ -46,6 +46,56 @@ INSERT INTO `tb_barang` (`id_brg`, `nama_brg`, `keterangan`, `kategori`, `harga`
 (4, 'sandal jepit HD 4K', 'sandal jepit HD', 'sepatu', 11000, 90, 'sepatu.jpg'),
 (7, 'sandal swallow', 'swallow', 'sandal', 80000, 543, 'wallpaperbetter.jpg'),
 (8, 'sandal jepit', 'sandal jepit', 'sandal', 99999, 10, 'wallpaperflare_com_wallpaper_(2).jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_invoice`
+--
+
+CREATE TABLE `tb_invoice` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(56) NOT NULL,
+  `alamat` varchar(225) NOT NULL,
+  `tgl_pesan` datetime NOT NULL,
+  `batas_bayar` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_invoice`
+--
+
+INSERT INTO `tb_invoice` (`id`, `nama`, `alamat`, `tgl_pesan`, `batas_bayar`) VALUES
+(1, 'Muhammad Roynaldi', 'Rembang', '2023-03-12 18:32:25', '2023-03-13 18:32:25'),
+(2, 'Samuel Andrey', 'Pa', '2023-03-12 18:35:41', '2023-03-13 18:35:41'),
+(3, 'Samuel Andrey', 'Pa', '2023-03-12 22:02:26', '2023-03-13 22:02:26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_pesanan`
+--
+
+CREATE TABLE `tb_pesanan` (
+  `id` int(11) NOT NULL,
+  `id_invoice` int(11) NOT NULL,
+  `id_brg` int(11) NOT NULL,
+  `nama_brg` varchar(50) NOT NULL,
+  `jumlah` int(3) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `pilihan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_pesanan`
+--
+
+INSERT INTO `tb_pesanan` (`id`, `id_invoice`, `id_brg`, `nama_brg`, `jumlah`, `harga`, `pilihan`) VALUES
+(1, 1, 1, 'Convers m71', 1, 400000, ''),
+(2, 1, 4, 'sandal jepit HD 4K', 1, 11000, ''),
+(3, 1, 7, 'sandal swallow', 1, 80000, ''),
+(4, 1, 8, 'sandal jepit', 1, 99999, ''),
+(5, 2, 1, 'Convers m71', 2, 400000, '');
 
 --
 -- Indexes for dumped tables
@@ -58,6 +108,18 @@ ALTER TABLE `tb_barang`
   ADD PRIMARY KEY (`id_brg`);
 
 --
+-- Indexes for table `tb_invoice`
+--
+ALTER TABLE `tb_invoice`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_pesanan`
+--
+ALTER TABLE `tb_pesanan`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -66,6 +128,18 @@ ALTER TABLE `tb_barang`
 --
 ALTER TABLE `tb_barang`
   MODIFY `id_brg` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `tb_invoice`
+--
+ALTER TABLE `tb_invoice`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tb_pesanan`
+--
+ALTER TABLE `tb_pesanan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
