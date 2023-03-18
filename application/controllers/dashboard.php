42826e52378b818c1108f259dfebcd113aad656c
RanggaDwi16 <112050709+RanggaDwi16@users.noreply.github.com>
2023-03-18 16:30:53 +0700

    Update dashboard.php

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -2,13 +2,16 @@
 
 class Dashboard extends CI_Controller {
 
-    public function index() 
-    {
-        $data['barang'] = $this->model_barang->tampil_data()->result();
-        $this->load->view('templates/header');
-        $this->load->view('templates/sidebar');
-        $this->load->view('dashboard', $data);
-        $this->load->view('templates/footer');
+    public function __construct(){
+        parent::__construct();
+
+        if($this->session->userdata('role_id') != '2'){
+            $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+            Anda belum Login!
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"<span aria-hidden="true">&times;</span></button>
+            </div>');
+            redirect('auth/login');
+        }
     }
 
     public function tambah_ke_keranjang($id)
