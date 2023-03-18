e47fb6ac362e3a411b95623bfc07eb285c141480
RanggaDwi16 <112050709+RanggaDwi16@users.noreply.github.com>
2023-03-18 16:54:19 +0700

    Update dashboard_admin.php

--- application/controllers/admin/dashboard_admin.php
+++ application/controllers/admin/dashboard_admin.php
@@ -5,7 +5,7 @@ class Dashboard_admin extends CI_Controller {
 	public function __construct(){
         parent::__construct();
 
-        if($this->session->userdata('role_id') != '2'){
+        if($this->session->userdata('role_id') != '1'){
             $this->session->set_flashdata('pesan', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
             Anda belum Login!
             <button type="button" class="close" data-dismiss="alert" aria-label="Close"<span aria-hidden="true">&times;</span></button>
