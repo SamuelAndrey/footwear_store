90c18e58df62d0a1cf6e6031821c520e2946dc25
RanggaDwi16 <112050709+RanggaDwi16@users.noreply.github.com>
2023-03-18 16:15:37 +0700

    Update footwear_store.sql
    Update Table User

--- footwear_store.sql
+++ footwear_store.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 12, 2023 at 04:03 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 7.4.30
+-- Generation Time: Mar 18, 2023 at 10:14 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,14 +35,14 @@ CREATE TABLE `tb_barang` (
   `harga` int(11) NOT NULL,
   `stok` int(4) NOT NULL,
   `gambar` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_barang`
 --
 
 INSERT INTO `tb_barang` (`id_brg`, `nama_brg`, `keterangan`, `kategori`, `harga`, `stok`, `gambar`) VALUES
-(1, 'Convers m71', 'sepatu convers all star', 'sepatu', 400000, 10, 'sepatu.jpg'),
+(1, 'Convers m71', 'sepatu convers all star', 'sepatu', 400000, 8, 'sepatu.jpg'),
 (4, 'sandal jepit HD 4K', 'sandal jepit HD', 'sepatu', 11000, 90, 'sepatu.jpg'),
 (7, 'sandal swallow', 'swallow', 'sandal', 80000, 543, 'wallpaperbetter.jpg'),
 (8, 'sandal jepit', 'sandal jepit', 'sandal', 99999, 10, 'wallpaperflare_com_wallpaper_(2).jpg');
@@ -59,7 +59,7 @@ CREATE TABLE `tb_invoice` (
   `alamat` varchar(225) NOT NULL,
   `tgl_pesan` datetime NOT NULL,
   `batas_bayar` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_invoice`
@@ -68,7 +68,11 @@ CREATE TABLE `tb_invoice` (
 INSERT INTO `tb_invoice` (`id`, `nama`, `alamat`, `tgl_pesan`, `batas_bayar`) VALUES
 (1, 'Muhammad Roynaldi', 'Rembang', '2023-03-12 18:32:25', '2023-03-13 18:32:25'),
 (2, 'Samuel Andrey', 'Pa', '2023-03-12 18:35:41', '2023-03-13 18:35:41'),
-(3, 'Samuel Andrey', 'Pa', '2023-03-12 22:02:26', '2023-03-13 22:02:26');
+(3, 'Samuel Andrey', 'Pa', '2023-03-12 22:02:26', '2023-03-13 22:02:26'),
+(4, '', '', '2023-03-15 16:14:44', '2023-03-16 16:14:44'),
+(5, '', '', '2023-03-15 16:16:02', '2023-03-16 16:16:02'),
+(6, '', '', '2023-03-15 16:17:40', '2023-03-16 16:17:40'),
+(7, '', '', '2023-03-15 16:19:17', '2023-03-16 16:19:17');
 
 -- --------------------------------------------------------
 
@@ -84,7 +88,7 @@ CREATE TABLE `tb_pesanan` (
   `jumlah` int(3) NOT NULL,
   `harga` int(10) NOT NULL,
   `pilihan` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_pesanan`
@@ -95,7 +99,42 @@ INSERT INTO `tb_pesanan` (`id`, `id_invoice`, `id_brg`, `nama_brg`, `jumlah`, `h
 (2, 1, 4, 'sandal jepit HD 4K', 1, 11000, ''),
 (3, 1, 7, 'sandal swallow', 1, 80000, ''),
 (4, 1, 8, 'sandal jepit', 1, 99999, ''),
-(5, 2, 1, 'Convers m71', 2, 400000, '');
+(5, 2, 1, 'Convers m71', 2, 400000, ''),
+(9, 7, 1, 'Convers m71', 2, 400000, '');
+
+--
+-- Triggers `tb_pesanan`
+--
+DELIMITER $$
+CREATE TRIGGER `pesanan_penjualan` AFTER INSERT ON `tb_pesanan` FOR EACH ROW BEGIN
+	UPDATE tb_barang SET stok = stok-NEW.jumlah
+    WHERE id_brg = NEW.id_brg;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `role_id` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id`, `nama`, `username`, `password`, `role_id`) VALUES
+(1, 'admin', 'admin', '123', 1),
+(2, 'user', 'user', '123', 2),
+(3, 'rangga', 'rangga', '1234', 2);
 
 --
 -- Indexes for dumped tables
@@ -120,6 +159,12 @@ ALTER TABLE `tb_pesanan`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -133,13 +178,19 @@ ALTER TABLE `tb_barang`
 -- AUTO_INCREMENT for table `tb_invoice`
 --
 ALTER TABLE `tb_invoice`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tb_pesanan`
 --
 ALTER TABLE `tb_pesanan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
