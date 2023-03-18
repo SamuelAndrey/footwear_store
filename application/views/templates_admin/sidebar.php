62ffecd5596c2d0452a459065e7f3a74a7f827ef
RanggaDwi16 <112050709+RanggaDwi16@users.noreply.github.com>
2023-03-18 16:48:29 +0700

    Add files via upload

--- application/views/templates_admin/sidebar.php
+++ application/views/templates_admin/sidebar.php
@@ -103,6 +103,21 @@
                             </div>
                         </li>
 
+                        <div class="navbar">
+
+                            <div class="topbar-divider d-none d-sm-block"></div>
+    
+                            <ul class="na navbar-nav navbar-right">
+                                    <?php if($this->session->userdata('username')){ ?>
+                                        <li><div>Selamat Datang <?php echo $this->session->userdata('username') ?></div></li>
+                                        <li class="ml-2"><?php echo anchor('auth/logout','Logout')?></li>
+                                    <?php }else{ ?>
+                                        <li><?php echo anchor('auth/login', 'Login');?></li>
+                                    <?php } ?> 
+    
+                                </ul>
+                        </div>
+
                         <!-- Nav Item - Alerts -->
                         <li class="nav-item dropdown no-arrow mx-1">
                             <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
@@ -220,38 +235,6 @@
                             </div>
                         </li>
 
-                        <div class="topbar-divider d-none d-sm-block"></div>
-
-                        <!-- Nav Item - User Information -->
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
-                                <img class="img-profile rounded-circle"
-                                    src="<?= base_url()?>assets/img/undraw_profile_1.svg">
-                            </a>
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profile
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Settings
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Activity Log
-                                </a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
-                                </a>
-                            </div>
-                        </li>
 
                     </ul>
 
