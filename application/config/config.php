0643819ce937d37e0b58170f5bd81675a40e8eb5
samuel <samuelandrey670@gmail.com>
2023-05-28 14:47:47 +0700

    Config for php 7

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://localhost/footwear_store/';
-$config['base_url'] = 'http://localhost:8074/footwear_store/'; // Khusus Samuel
+$config['base_url'] = 'http://localhost/footwear_store/';
+// $config['base_url'] = 'http://localhost:8074/footwear_store/'; // Khusus Samuel
 
 /*
 |--------------------------------------------------------------------------
