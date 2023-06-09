7ff5ace158eccdaf01caf83e921833c5a2037ba9
samuel <samuelandrey670@gmail.com>
2023-06-09 20:32:11 +0700

    Update database

--- footwear_store.sql
+++ footwear_store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 08 Jun 2023 pada 01.29
+-- Waktu pembuatan: 09 Jun 2023 pada 20.31
 -- Versi server: 10.4.27-MariaDB
 -- Versi PHP: 8.2.0
 
@@ -32,7 +32,6 @@ USE `footwear_store`;
 CREATE TABLE `tb_barang` (
   `id_brg` int(11) NOT NULL,
   `nama_brg` varchar(120) NOT NULL,
-  `keterangan` text NOT NULL,
   `kategori` varchar(60) NOT NULL,
   `harga` int(11) NOT NULL,
   `stok` int(4) NOT NULL,
@@ -43,12 +42,12 @@ CREATE TABLE `tb_barang` (
 -- Dumping data untuk tabel `tb_barang`
 --
 
-INSERT INTO `tb_barang` (`id_brg`, `nama_brg`, `keterangan`, `kategori`, `harga`, `stok`, `gambar`) VALUES
-(1, 'Convers m71', 'sepatu convers all star', 'sepatu', 400000, 2, 'sepatu.jpg'),
-(4, 'sandal jepit HD 4K', 'sandal jepit HD', 'sepatu', 11000, 88, 'sepatu.jpg'),
-(7, 'sandal swallow', 'swallow', 'sandal', 80000, 540, 'wallpaperbetter.jpg'),
-(8, 'sandal jepit', 'sandal jepit', 'sandal', 99999, 8, 'wallpaperflare_com_wallpaper_(2).jpg'),
-(9, 'kulkas', 'kulkas 3 pintu lg', 'Sepatu', 9000000, 3, 'mobile_(2).png');
+INSERT INTO `tb_barang` (`id_brg`, `nama_brg`, `kategori`, `harga`, `stok`, `gambar`) VALUES
+(1, 'Convers m71', 'sepatu', 400000, 1, 'sepatu.jpg'),
+(4, 'sandal jepit HD 4K', 'sepatu', 11000, 88, 'sepatu.jpg'),
+(7, 'sandal swallow', 'sandal', 80000, 540, 'wallpaperbetter.jpg'),
+(8, 'sandal jepit', 'sandal', 99999, 8, 'wallpaperflare_com_wallpaper_(2).jpg'),
+(9, 'kulkas', 'Sepatu', 9000000, 3, 'mobile_(2).png');
 
 -- --------------------------------------------------------
 
@@ -58,6 +57,7 @@ INSERT INTO `tb_barang` (`id_brg`, `nama_brg`, `keterangan`, `kategori`, `harga`
 
 CREATE TABLE `tb_invoice` (
   `id` int(11) NOT NULL,
+  `id_member` int(11) NOT NULL,
   `nama` varchar(56) NOT NULL,
   `alamat` varchar(225) NOT NULL,
   `tgl_pesan` datetime NOT NULL,
@@ -66,16 +66,37 @@ CREATE TABLE `tb_invoice` (
   `bank` varchar(100) NOT NULL,
   `status_konfirmasi` int(1) NOT NULL,
   `no_telepon` varchar(20) NOT NULL,
-  `ongkir` int(11) NOT NULL
+  `ongkir` int(11) NOT NULL,
+  `total_bayar` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `tb_invoice`
 --
 
-INSERT INTO `tb_invoice` (`id`, `nama`, `alamat`, `tgl_pesan`, `batas_bayar`, `no_rekening`, `bank`, `status_konfirmasi`, `no_telepon`, `ongkir`) VALUES
-(11, 'Rizal Pratama', 'Banjir kanal SMG, RT 69 RW 02, Jawa Tengah 50123', '2023-06-08 01:13:38', '2023-06-09 01:13:38', '012 90987649', 'BCA', 1, '082345678901', 0),
-(12, 'Samuel Andrey Aji Prasetya', 'Los Santos Grove Street, RT 10 RW 90', '2023-06-08 01:15:25', '2023-06-09 01:15:25', '089 93918263', 'BRI', 0, '082221112223', 0);
+INSERT INTO `tb_invoice` (`id`, `id_member`, `nama`, `alamat`, `tgl_pesan`, `batas_bayar`, `no_rekening`, `bank`, `status_konfirmasi`, `no_telepon`, `ongkir`, `total_bayar`) VALUES
+(13, 1, 'Samuel Andrey Aji Prasetya', 'Ds konoha', '2023-06-09 16:53:49', '2023-06-10 16:53:49', '0987 1234452345', 'BNI', 1, '08123234212', 0, 420000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_member`
+--
+
+CREATE TABLE `tb_member` (
+  `id_member` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_member`
+--
+
+INSERT INTO `tb_member` (`id_member`, `nama`, `email`, `username`, `password`) VALUES
+(1, 'Samuel Andrey Aji Prasetya', 'samuelandrey@gmail.com', 'Samuel Andrey', '123');
 
 -- --------------------------------------------------------
 
@@ -102,7 +123,8 @@ INSERT INTO `tb_pesanan` (`id`, `id_invoice`, `id_brg`, `nama_brg`, `jumlah`, `h
 (18, 11, 7, 'sandal swallow', 1, 80000, ''),
 (19, 12, 8, 'sandal jepit', 1, 99999, ''),
 (20, 12, 9, 'kulkas', 1, 9000000, ''),
-(21, 12, 1, 'Convers m71', 1, 400000, '');
+(21, 12, 1, 'Convers m71', 1, 400000, ''),
+(22, 13, 1, 'Convers m71', 1, 400000, '');
 
 --
 -- Trigger `tb_pesanan`
@@ -134,11 +156,7 @@ CREATE TABLE `tb_user` (
 --
 
 INSERT INTO `tb_user` (`id`, `nama`, `username`, `password`, `role_id`) VALUES
-(1, 'admin', 'admin', '123', 1),
-(2, 'user', 'user', '123', 2),
-(3, 'rangga', 'rangga', '1234', 2),
-(4, 'samuel', 'samuel andrey', '123', 2),
-(5, 'samuel andrey', 'samuel', '123', 2);
+(1, 'admin', 'admin', '123', 1);
 
 --
 -- Indexes for dumped tables
@@ -155,6 +173,12 @@ ALTER TABLE `tb_barang`
 --
 ALTER TABLE `tb_invoice`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_member`
+--
+ALTER TABLE `tb_member`
+  ADD PRIMARY KEY (`id_member`);
 
 --
 -- Indeks untuk tabel `tb_pesanan`
@@ -182,13 +206,19 @@ ALTER TABLE `tb_barang`
 -- AUTO_INCREMENT untuk tabel `tb_invoice`
 --
 ALTER TABLE `tb_invoice`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_member`
+--
+ALTER TABLE `tb_member`
+  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_pesanan`
 --
 ALTER TABLE `tb_pesanan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT untuk tabel `tb_user`
